c1e8baa62776a55148c60f00e4f5863fa9a94f56
DaryllCulas <culasdaryll99@gmail.com>
2024-03-13 18:51:46 +0800

    Add pet matchmaking module

---
+++ petfaves/lib/pet_match_making/pet_match_making_module.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+class PetMatchMaking extends StatefulWidget {
+  const PetMatchMaking({super.key});
+
+  @override
+  _PetMatchMakingState createState() => _PetMatchMakingState();
+}
+
+class _PetMatchMakingState extends State<PetMatchMaking> {
+  // int _currentIndex = 0;
+  final List<String> _images = [
+    'https://as2.ftcdn.net/v2/jpg/01/99/00/79/1000_F_199007925_NolyRdRrdYqUAGdVZV38P4WX8pYfBaRP.jpg',
+    'https://as1.ftcdn.net/v2/jpg/04/81/85/46/1000_F_481854656_gHGTnBscKXpFEgVTwAT4DL4NXXNhDKU9.jpg',
+    'https://as1.ftcdn.net/v2/jpg/02/13/96/52/1000_F_213965280_VZZncwRZDWcfH2yX1glTdq34xiN4kzA5.jpg',
+    // Add more image URLs as needed
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CarouselSlider(
+          options: CarouselOptions(height: 400.0),
+          items: _images.map((imageUrl) {
+            return Container(
+              width: MediaQuery.of(context).size.width,
+              margin: const EdgeInsets.symmetric(horizontal: 5.0),
+              decoration: BoxDecoration(
+                color: Colors.amber,
+                borderRadius: BorderRadius.circular(
+                    10.0), // Optional: Add border radius for card-like appearance
+              ),
+              child: Image.network(
+                imageUrl,
+                fit: BoxFit.cover,
+              ),
+            );
+          }).toList(),
+        ),
+        // Additional widgets or content can be added here
+      ],
+    );
+  }
+}
