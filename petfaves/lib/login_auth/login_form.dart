905a3cca5eed81df2d99ddad3b53924f311f2543
DaryllCulas <culasdaryll99@gmail.com>
2024-03-22 01:06:46 +0800

    modified

--- petfaves/lib/login_auth/login_form.dart
+++ petfaves/lib/login_auth/login_form.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: use_build_context_synchronously
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
@@ -47,9 +45,10 @@ class _LoginPageState extends State<LoginPage> {
 
         // Navigate to the next screen if login is successful
         Navigator.push(
+          // ignore: use_build_context_synchronously
           context,
           MaterialPageRoute(
-            builder: (context) => PetFeeds(),
+            builder: (context) => const PetFeeds(),
           ),
         );
       }
