77e75cda507118523d65264b289c07244248d3f5
DaryllCulas <culasdaryll99@gmail.com>
2024-03-22 01:07:12 +0800

    Modified

--- petfaves/lib/main.dart
+++ petfaves/lib/main.dart
@@ -26,7 +26,7 @@ class _MyAppState extends State<MyApp> {
         scaffoldBackgroundColor: const Color.fromARGB(255, 221, 218, 218),
       ),
       debugShowCheckedModeBanner: false,
-      home: AuthPage(),
+      home: const AuthPage(),
     );
   }
 }
