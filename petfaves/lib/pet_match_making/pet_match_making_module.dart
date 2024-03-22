929d8347859bd824e7e61320eee06f9984cdaf7c
DaryllCulas <culasdaryll99@gmail.com>
2024-03-22 19:55:01 +0800

    modified

--- petfaves/lib/pet_match_making/pet_match_making_module.dart
+++ petfaves/lib/pet_match_making/pet_match_making_module.dart
@@ -103,7 +103,7 @@ class _PetMatchMakingState extends State<PetMatchMaking> {
                                   color: Colors.black.withOpacity(0.2),
                                   spreadRadius: 2,
                                   blurRadius: 7,
-                                  offset: const Offset(0, 3),
+                                  offset: const Offset(0, 5),
                                 )
                               ],
                             ),
