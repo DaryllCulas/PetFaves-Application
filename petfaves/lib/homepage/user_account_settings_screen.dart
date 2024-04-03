e480e75f79c9448a3e7908779ad3cc9e1a5c9783
DaryllCulas <culasdaryll99@gmail.com>
2024-04-03 18:50:59 +0800

    Add user account settings screen

---
+++ petfaves/lib/homepage/user_account_settings_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class UserSettingScreen extends StatefulWidget {
+  const UserSettingScreen({super.key});
+
+  @override
+  State<UserSettingScreen> createState() => _UserSettingScreenState();
+}
+
+class _UserSettingScreenState extends State<UserSettingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Account Settings'),
+      ),
+      body: const Center(
+        child: Text(
+          'Users Account Settings',
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+      ),
+    );
+  }
+}
