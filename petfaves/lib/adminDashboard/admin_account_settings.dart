356e6d9d641de75ba14e0ac75a71eb9dda1e6368
DaryllCulas <culasdaryll99@gmail.com>
2024-04-03 18:47:50 +0800

    admin account settings screen added

---
+++ petfaves/lib/adminDashboard/admin_account_settings.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class AccountSettings extends StatefulWidget {
+  const AccountSettings({super.key});
+
+  @override
+  State<AccountSettings> createState() => _AccountSettingsState();
+}
+
+class _AccountSettingsState extends State<AccountSettings> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Account Settings'),
+      ),
+      body: const Center(
+        child: Text(
+          'Account Settings Screen',
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+    );
+  }
+}
