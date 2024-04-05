76a0e8c86179cc614d4b542edc79070681d718d9
DaryllCulas <culasdaryll99@gmail.com>
2024-04-05 09:08:05 +0800

    floating action button functionality fixed

--- petfaves/lib/components/expandable_fab.dart
+++ petfaves/lib/components/expandable_fab.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:petfaves/components/pet_preferences_questionnaire_form.dart';
+import 'package:petfaves/components/user_notification.dart';
+import 'package:petfaves/components/user_post_content.dart';
 
 class ExpandableFab extends StatefulWidget {
   @override
@@ -15,6 +18,7 @@ class _ExpandableFabState extends State<ExpandableFab> {
       mainAxisAlignment: MainAxisAlignment.end,
       children: [
         FloatingActionButton(
+          key: UniqueKey(), // Add a unique key to the FloatingActionButton
           onPressed: () {
             setState(() {
               _isExpanded = !_isExpanded;
@@ -29,8 +33,15 @@ class _ExpandableFabState extends State<ExpandableFab> {
         SizedBox(height: _isExpanded ? 10.0 : 0),
         if (_isExpanded) ...[
           FloatingActionButton.extended(
+            heroTag: 'AnswerForm',
             onPressed: () {
               // Action for Answer Form icon
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const PetPreferencesQuestionnaire(),
+                ),
+              );
             },
             backgroundColor: Colors.blue,
             icon: const Icon(
@@ -38,14 +49,21 @@ class _ExpandableFabState extends State<ExpandableFab> {
               color: Colors.black,
             ),
             label: const Text(
-              'Answer Form',
+              'Fill out form',
               style: TextStyle(color: Colors.black),
             ),
           ),
           const SizedBox(height: 10.0),
           FloatingActionButton.extended(
+            heroTag: 'PostSomething',
             onPressed: () {
               // Action for Post something icon
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const UserPostContent(),
+                ),
+              );
             },
             backgroundColor: Colors.blue,
             icon: const Icon(Icons.post_add, color: Colors.black),
@@ -56,8 +74,15 @@ class _ExpandableFabState extends State<ExpandableFab> {
           ),
           const SizedBox(height: 10.0),
           FloatingActionButton.extended(
+            heroTag: 'Notifications',
             onPressed: () {
               // Action for message icon
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const UserNotifications(),
+                ),
+              );
             },
             backgroundColor: Colors.blue,
             icon: const Icon(Icons.notifications, color: Colors.black),
